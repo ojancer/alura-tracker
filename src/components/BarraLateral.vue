d1d11ca88c2c71341d84eb7e56cdd3c8fd7db19a
Osvaldo Jancer <osvado.jancer@bling.com.br>
2025-12-02 22:13:55 -0300

    Aplicando o compontente TaskForm

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>
+            Alura Tracker
+            <i class="fa-solid fa-location-crosshairs"></i>
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BarraLateral',
+});
+</script>
+
+<style scoped> 
+    header {
+        padding: 1rem;
+        background: #0d3b66;
+        width: 100%;
+        height: 100vh;
+        }
+    @media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+    }
+    h1 {
+        color: #faf0ca;
+        font-size: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+    }
+</style>
