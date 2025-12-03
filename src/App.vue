98128e3d4ce789e27be13fc4486803539f80a215
Osvaldo Jancer <osvado.jancer@bling.com.br>
2025-12-02 22:11:17 -0300

    Aplicando os primeiros compontens e ajustando os arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+   <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <TaskForm />
+    </div>
+  </main>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import TaskForm from './components/TaskForm.vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    TaskForm 
+  }   
+});
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  main {
+    height: 100vh;
+  }
 </style>
--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>hello world</h1>
-  </div>
-</template>
