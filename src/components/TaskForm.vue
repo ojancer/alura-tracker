8495420a045a986d8a52271af48a5beacecb1fc2
Osvaldo Jancer <osvado.jancer@bling.com.br>
2025-12-09 20:52:41 -0300

    criação dos componentes de formulário e AppCronometro]

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -6,16 +6,14 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>00:00:00</strong>
-                    </section>
-                    <button class="button">
+                   <AppCronometro :tempoEmSegundos="tempoEmSegundos" />
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -29,9 +27,33 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import AppCronometro from './AppCronometro.vue';
 
 export default defineComponent({
     name: 'TaskForm',
+    components: {
+        AppCronometro
+    },
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: null as ReturnType<typeof setInterval> | null
+        };
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+        }, 
+        finalizar() { 
+            if (this.cronometro) {
+            clearInterval(this.cronometro)
+            this.cronometro = null;
+            this.tempoEmSegundos = 0;
+            }
+        }
+    }    
 });
 </script>
 
